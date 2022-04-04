59aa392afc3d301741b50b3ff15481c88cc76206
Alexey <lxmlshv97@mail.ru>
2022-04-04 22:30:27 +0300

    delete pipe webp from scss.js

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -31,17 +31,6 @@ export const scss = () => {
 			})
 			)
 		)
-	.pipe(
-		app.plugins.if(
-			app.isBuild,
-			webpcss(
-			{
-				webpClass: ".webp",
-				noWebpClass: ".no-webp"
-			}
-			)
-			)
-		)
 		// Раскомментировать если нужен не сжатый дубль файла стилей
 		// .pipe(app.gulp.dest(app.path.build.css))
 		.pipe(
